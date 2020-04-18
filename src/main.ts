452828270b87d94d49b21d5e4314c1276d1a48cc
Xavi Fuentes <xavif@nandos.co.uk>
2020-04-19 00:01:13 +0100

    add Docker development

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { SERVER_PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
